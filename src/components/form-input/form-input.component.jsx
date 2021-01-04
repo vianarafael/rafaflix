92422d98f05ad1bf67a70d9ec02de7b13eb29991
vianarafael <vianarafaelds@gmail.com>
2021-01-04 11:38:39 +0900

    fixed the logic of the labels on signin/registration

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useRef } from 'react';
 
-import "./form-input.styles.scss";
-
-const FormInput = ({ handleChange, label, ...otherProps }) => (
-  <div className="group">
-    <input className="form-input" onChange={handleChange} {...otherProps} />
-    {label ? (
-      <label className={`{props.value.length? 'shrink': ''} form-input-label`}>
-        {label}
-      </label>
-    ) : null}
-  </div>
-);
+import './form-input.styles.scss';
+const FormInput = ({ handleChange, label, ...otherProps }) => {
+  const inputEl = useRef(null);
+  if (inputEl.current) {
+    console.log(inputEl.current.value);
+  }
+  return (
+    <div className="group">
+      <input
+        ref={inputEl}
+        className="form-input"
+        onChange={handleChange}
+        {...otherProps}
+      />
+      {label ? (
+        <label
+          className={`${
+            inputEl.current && inputEl.current.value.length ? 'shrink' : ''
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      ) : null}
+    </div>
+  );
+};
 
 export default FormInput;
